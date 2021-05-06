122bf4000eadc723a524f22a0143cd5efd5ac4ee
AhmedBenMakhlouf <ahmed.benmakhlouf@yahoo.fr>
2021-05-07 00:36:00 +0200

    update

--- src/Components/Navbar/Navabar.js
+++ src/Components/Navbar/Navabar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import {
   Button,
@@ -18,134 +19,174 @@ import {
   UncontrolledTooltip,
 } from "reactstrap";
 
+class Navbars extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    username:"",
+    password:"" ,
+    passwordFocus:false,
+    emailFocus:false,
+    collapseOpen:false,
+    collapseOut:'',
+    color:'navbar-transparent'
+       
+  };
+}
 
-export default function Navbars() {
-    const [collapseOpen, setCollapseOpen] = React.useState(false);
-    const [collapseOut, setCollapseOut] = React.useState("");
-    const [color, setColor] = React.useState("navbar-transparent");
-    React.useEffect(() => {
-      window.addEventListener("scroll", changeColor);
-      return function cleanup() {
-        window.removeEventListener("scroll", changeColor);
-      };
-    },[]);
-    const changeColor = () => {
-      if (
-        document.documentElement.scrollTop > 99 ||
-        document.body.scrollTop > 99
-      ) {
-        setColor("bg-info");
-      } else if (
-        document.documentElement.scrollTop < 100 ||
-        document.body.scrollTop < 100
-      ) {
-        setColor("navbar-transparent");
-      }
-    };
-    const toggleCollapse = () => {
-      document.documentElement.classList.toggle("nav-open");
-      setCollapseOpen(!collapseOpen);
-    };
-    const onCollapseExiting = () => {
-      setCollapseOut("collapsing-out");
-    };
-    const onCollapseExited = () => {
-      setCollapseOut("");
-    };
-    const scrollToDownload = () => {
-      document
-        .getElementById("download-section")
-        .scrollIntoView({ behavior: "smooth" });
-    };
-    return (
-      <Navbar className={"fixed-top " + color} color-on-scroll="100" expand="lg">
-        <Container>
-          <div className="navbar-translate">
-            <NavbarBrand to="/" tag={Link} id="navbar-brand">
-              <span>shifti</span>
-            </NavbarBrand>
-            <button
-              aria-expanded={collapseOpen}
-              className="navbar-toggler navbar-toggler"
-              onClick={toggleCollapse}
-            >
-              <span className="navbar-toggler-bar bar1" />
-              <span className="navbar-toggler-bar bar2" />
-              <span className="navbar-toggler-bar bar3" />
-            </button>
-          </div>
-          <Collapse
-            className={"justify-content-end " + collapseOut}
-            navbar
-            isOpen={collapseOpen}
-            onExiting={onCollapseExiting}
-            onExited={onCollapseExited}
-          >
-            <div className="navbar-collapse-header">
-              <Row>
-                <Col className="collapse-brand" xs="6">
-                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                   Shifti
-                  </a>
-                </Col>
-                <Col className="collapse-close text-right" xs="6">
-                  <button
-                    aria-expanded={collapseOpen}
-                    className="navbar-toggler"
-                    onClick={toggleCollapse}
-                  >
-                    <i className="tim-icons icon-simple-remove" />
-                  </button>
-                </Col>
-              </Row>
-            </div>
-            <Nav navbar>
-              <UncontrolledDropdown nav>
-                <DropdownToggle
-                  caret
-                  color="default"
-                  data-toggle="dropdown"
-                  href="#pablo"
-                  nav
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fa fa-cogs d-lg-none d-xl-none" />
-                  Getting started
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-with-icons">
-                  <DropdownItem tag={Link} to="/register">
-                    <i className="tim-icons icon-bullet-list-67" />
-                    Register
-                  </DropdownItem>
-                  <DropdownItem tag={Link} to="/espace-client">
-                    <i className="tim-icons icon-single-02" />
-                    Profile
-                  </DropdownItem>
-                  <DropdownItem tag={Link} to="/template">
-                    <i className="tim-icons icon-single-02" />
-                    Template
-                  </DropdownItem>
-                  <DropdownItem tag={Link} to="/offre">
-                    <i className="tim-icons icon-single-02" />
-                    Offre
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem>
-                <Button
-                  className="nav-link d-none d-lg-block"
-                  color="primary"
-                  target="_blank"
-                >
-                  <Link to="/login">
-                  <i className="tim-icons icon-spaceship" />Login
-                  </Link> 
-                </Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-    );
+
+
+
+componentDidMount(){
+  window.addEventListener("scroll", this.changeColor);
+  return function cleanup() {
+    window.removeEventListener("scroll", this.changeColor);
+  };
+}
+
+ changeColor = () => {
+  if (
+    document.documentElement.scrollTop > 99 ||
+    document.body.scrollTop > 99
+  ) {
+    
+    this.setState({color:'bg-info'})
+  } else if (
+    document.documentElement.scrollTop < 100 ||
+    document.body.scrollTop < 100
+  ) {
+    this.setState({color:'navbar-transparent'})
+    
   }
+};
+ toggleCollapse = () => {
+  document.documentElement.classList.toggle("nav-open");
+  this.setState({collapseOpen:true})
+};
+ onCollapseExiting = () => {
+
+  this.setState({collapseOut:"collapsing-out"})
+};
+ onCollapseExited = () => {
+ 
+  this.setState({collapseOut:''})
+};
+
+
+
+
+scrollToDownload = () => {
+  document
+    .getElementById("download-section")
+    .scrollIntoView({ behavior: "smooth" });
+};
+render(){
+ console.log("username", this.props.auth.client.user.firstName);
+  return (
+    <Navbar className={"fixed-top " + this.state.color} color-on-scroll="100" expand="lg">
+      <Container>
+        <div className="navbar-translate">
+          <NavbarBrand to="/" tag={Link} id="navbar-brand">
+            <span>shifti</span>
+          </NavbarBrand>
+          <button
+            aria-expanded={this.state.collapseOpen}
+            className="navbar-toggler navbar-toggler"
+            onClick={this.toggleCollapse}
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className={"justify-content-end " + this.state.collapseOut}
+          navbar
+          isOpen={this.state.collapseOpen}
+          onExiting={this.onCollapseExiting}
+          onExited={this.onCollapseExited}
+        >
+          <div className="navbar-collapse-header">
+            <Row>
+              <Col className="collapse-brand" xs="6">
+                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                 Shifti
+                </a>
+              </Col>
+              <Col className="collapse-close text-right" xs="6">
+                <button
+                  aria-expanded={this.state.collapseOpen}
+                  className="navbar-toggler"
+                  onClick={this.toggleCollapse}
+                >
+                  <i className="tim-icons icon-simple-remove" />
+                </button>
+              </Col>
+            </Row>
+          </div>
+          <Nav navbar>
+            <UncontrolledDropdown nav>
+              <DropdownToggle
+                caret
+                color="default"
+                data-toggle="dropdown"
+                href="#pablo"
+                nav
+                onClick={(e) => e.preventDefault()}
+              >
+                <i className="fa fa-cogs d-lg-none d-xl-none" />
+                Getting started
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-with-icons">
+                <DropdownItem tag={Link} to="/register">
+                  <i className="tim-icons icon-bullet-list-67" />
+                  Register
+                </DropdownItem>
+                <DropdownItem tag={Link} to="/espace-client">
+                  <i className="tim-icons icon-single-02" />
+                  Profile
+                </DropdownItem>
+                <DropdownItem tag={Link} to="/template">
+                  <i className="tim-icons icon-single-02" />
+                  Template
+                </DropdownItem>
+                <DropdownItem tag={Link} to="/offre">
+                  <i className="tim-icons icon-single-02" />
+                  Offre
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <Button
+                className="nav-link d-none d-lg-block"
+                color="primary"
+                target="_blank"
+              >
+                {this.props.auth && this.props.auth.client?
+                <div>{"hello again "+this.props.auth.client.user.firstName+" "+ this.props.auth.client.user.lastName}</div>:<Link to="/login"><i className="tim-icons icon-spaceship" />Login</Link>  }
+              </Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  auth: state.auth
+})
+
+
+const mapDispatchToProps = (dispatch) => {
+return {
+dispatch: (action) => {
+dispatch(action);
+},
+};
+};
+export default connect (mapStateToProps, mapDispatchToProps)(Navbars)
+
   
